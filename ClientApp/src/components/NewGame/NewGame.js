0898b126c9135d5b7873f9f752e852e7d66ed193
JohnJ-Led <jacob.ledthebox@gmail.com>
2020-12-20 16:31:03 -0600

    Add Basic Table to New Game

--- ClientApp/src/components/NewGame/NewGame.js
+++ ClientApp/src/components/NewGame/NewGame.js
@@ -15,7 +15,7 @@ export class NewGame extends Component {
             }],
             player: [{
                 pname: '',
-                score:''
+                score:['']
             }],
             newGame: false
         }
@@ -56,7 +56,54 @@ export class NewGame extends Component {
                             onChange={()=>this.handleOnChange} />
                     </label>
                 </form>
+                <table className='table table-striped' aria-labelledby="tabelLabel">
+                    <thead>
+                    <tr>
+                        <td> Hole:</td>
+                        <th scope="col"> 1 </th>
+                        <th scope="col"> 2 </th>
+                        <th scope="col"> 3 </th>
+                        <th scope="col"> 4 </th>
+                        <th scope="col"> 5 </th>
+                        <th scope="col"> 6 </th>
+                        <th scope="col"> 7 </th>
+                        <th scope="col"> 8 </th>
+                        <th scope="col"> 9 </th>
+                        <th scope="col"> Front </th>
+                        <th scope="col"> 10 </th>
+                        <th scope="col"> 11 </th>
+                        <th scope="col"> 12 </th>
+                        <th scope="col"> 13 </th>
+                        <th scope="col"> 14 </th>
+                        <th scope="col"> 15 </th>
+                        <th scope="col"> 16 </th>
+                        <th scope="col"> 17 </th>
+                        <th scope="col"> 18 </th>
+                        <th scope="col"> Total </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row"> Distance</th>
+                    </tr>
+                    <tr>
+                        <th scope="row"> Par</th>
+                        </tr>
+                    </tbody>
+                </table>
+
                 <br />
+                <table className='table table-striped' aria-labelledby="tabelLabel">
+                    <tbody>
+                        <tr>
+                            <th scrope="row"> Player: </th>
+                        </tr>
+                        <tr>
+                            <th scope="row"> SomeName </th>
+                        </tr>
+                    </tbody>
+                </table>
+
                 <br/>
                 <button className="btn btn-primary" onClick={()=>this.onSaveGame}>Save Game</button>
             </div>
