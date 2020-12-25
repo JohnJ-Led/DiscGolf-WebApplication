438e114a1fd90739c6d2477535fd1fcab02fb89b
JohnJ-Led <jacob.ledthebox@gmail.com>
2020-12-25 00:48:54 -0600

    NewGame Updates: Updatable Table Cells

--- ClientApp/src/components/NewGame/NewGame.js
+++ ClientApp/src/components/NewGame/NewGame.js
@@ -7,46 +7,150 @@ export class NewGame extends Component {
     constructor(props) {
         super(props);
         this.state = {
-                gameID: 'Game',
-                location: 'Place',
+            game:
+            {
+                gameID: '',
+                location: '',
                 hole: [{
                     holeNbr: '',
                     distance: '',
                     par: '',
                 }],
                 player: [{
+                    pid: '',
                     pname: '',
-                    score: ['']
-            }],
+                    score: []
+                }]
+            },
             newGame: false
         };
         this.isNewGame = this.isNewGame.bind(this);
-        this.handleOnChange = this.handleOnChange.bind(this);
+        this.handleOnChangeGame = this.handleOnChangeGame.bind(this);
         this.onSaveGame = this.onSaveGame.bind(this);
+        this.populateGameHead = this.populateGameHead.bind(this);
+        this.populatePlayer = this.populatePlayer.bind(this);
+        this.addNewPlayer = this.addNewPlayer.bind(this);
     }
 
     isNewGame() {
+        //Inititatly state setup
         this.setState({ newGame: true });
-        //this.setState({ holeNbr: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18']});
-        //console.log( this.state.holeNbr );
-        console.log("I got here");
+        let { game } = this.state;
+        let tempArry = [];
+        let scores = [];
+        for (let i = 1; i <= 18; i++) {
+            let holeObj = { holeNbr: i, distance: '', par: '' };
+            tempArry.push(holeObj);
+            scores.push('');
+        }
+        game["hole"] = tempArry;
+        game["player"] = [{ pid: 1, pname: '', score: scores }];
+        this.setState({ game });
+        console.log(tempArry);
+
     }
 
     onSaveGame() {
-         alert("You're Game has been save!");
+        console.log(this.state.game);
+        
     }
 
-    handleOnChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
+    handleOnChangeGame(e) {
+        //Use to update None Array state objects
+        let { game } = this.state;
+        game[e.target.name] = e.target.value;
         this.setState({
-            [name]: value
+            game
         });
     }
 
-    static populateNewGame() {
-        console.log("DO SOMETHING!!!!");
+    handleOnChangeHole = (id, e) => {
+        //Use to update Array state objects
+        let { game } = this.state;
+        if(e.target.name === 'distance' || e.target.name === 'par') {
+            let updateHole = [...game.hole];
+            updateHole[id - 1][e.target.name] = e.target.value;
+            game["hole"] = updateHole;
+        }
+        else if (e.target.name === 'pname' || e.target.name === 'score') {
+            let updatePlayer = [...game.player];
+            updatePlayer[id - 1][e.target.name] = e.target.value;
+                game["player"] = updatePlayer;
+
+        }
+        this.setState({ game });
+    }
+    populateGameHead() {
+
+        let hole = this.state.game.hole;
+        return (
+            <table className="table table-bordered" aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Hole:</th>
+                    {hole.map((hole) =>
+                        <th scope="col" key={hole.holeNbr} >
+                            {hole.holeNbr}
+                        </th >
+                    )}
+                </tr>
+                
+                    <tr>
+                        <th> Distance </th>
+                    {hole.map((hole) =>
+                        <td key={hole.holeNbr}><input type="text" name="distance" className="new-game-edit" onChange={(e) => this.handleOnChangeHole(hole.holeNbr, e)}/>
+                            </td>
+                        )}
+                    </tr>
+                        <tr>
+                            <th> Par </th>
+                    {hole.map((hole) =>
+                        <td  key={hole.holeNbr}><input type="text" name="par" className="new-game-edit" onChange={(e) => this.handleOnChangeHole(hole.holeNbr, e)}/>
+                            </td>
+                        )}
+                    </tr>
+                
+            </thead>
+            </table>
+        );
+    }
+
+    addNewPlayer() {
+        let { game } = this.state;
+        let scores = [];
+        for (let i = 0; i < 18; i++) {
+            scores.push('');
+        }
+
+        let addPlayer = [...game.player];
+        addPlayer.push({ pid: addPlayer.length+1, pname: '', score: scores });
+        game["player"] = addPlayer;
+        
+        this.setState({ game });
+    }
+
+    populatePlayer() {
+        let player = this.state.game.player;
+        return (
+            <table className='table table-bordered' aria-labelledby="tabelLabel">
+                <tbody>
+                    <tr>
+                        <th colSpan="20" scope="colgroup"> Player: </th>
+                        <th ><button className="btn btn-primary" onClick={this.addNewPlayer}>Add</button></th>
+                    </tr>
+                </tbody>
+                <tbody>
+                    {player.map((player) => 
+                            <tr key={player.pid}>
+                                <th><input type="text" name="pname" className="new-game-edit" onChange={(e) => this.handleOnChangeHole(player.pid, e)} /></th>
+                            </tr>
+                    )}
+                </tbody>
+            </table>
+            );
+    }
+
+    populateNewGame() {
         return (
             <div>
                 <form>
@@ -55,130 +159,21 @@ export class NewGame extends Component {
                         <input
                             name="gameID"
                             type="text"
-                            value={this.state.gameID}
-                            onChange={this.handleOnChange} />
+                            
+                            onChange={this.handleOnChangeGame} />
                     </label>
                     <label>
                         Location:
                         <input
                             name="location"
                             type="text"
-                            value={this.state.location}
-                            onChange={this.handleOnChange} />
+                            value={this.state.game.location}
+                            onChange={this.handleOnChangeGame} />
                     </label>
+                    
                 </form>
-                <table className='table table-striped' aria-labelledby="tabelLabel">
-                    <thead>
-                        <tr>
-                            <td> Hole:</td>
-                            <th scope="col"> 1 </th>
-                            <th scope="col"> 2 </th>
-                            <th scope="col"> 3 </th>
-                            <th scope="col"> 4 </th>
-                            <th scope="col"> 5 </th>
-                            <th scope="col"> 6 </th>
-                            <th scope="col"> 7 </th>
-                            <th scope="col"> 8 </th>
-                            <th scope="col"> 9 </th>
-                            <th scope="col"> Front </th>
-                            <th scope="col"> 10 </th>
-                            <th scope="col"> 11 </th>
-                            <th scope="col"> 12 </th>
-                            <th scope="col"> 13 </th>
-                            <th scope="col"> 14 </th>
-                            <th scope="col"> 15 </th>
-                            <th scope="col"> 16 </th>
-                            <th scope="col"> 17 </th>
-                            <th scope="col"> 18 </th>
-                            <th scope="col"> Total </th>
-                        </tr>
-                    </thead>
-                    <tbody className="table-bordered">
-                        <tr>
-                            <td>Distance</td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                            <td><input type="text" className="new-game-edit" /></td>
-                        </tr>
-                        <tr>
-                            <td> Par</td>
-                            <td> 1 </td>
-                            <td> 2 </td>
-                            <td> 3 </td>
-                            <td> 4 </td>
-                            <td> 5 </td>
-                            <td> 6 </td>
-                            <td> 7 </td>
-                            <td> 8 </td>
-                            <td> 9 </td>
-                            <td> 123 </td>
-                            <td> 10 </td>
-                            <td> 11 </td>
-                            <td> 12 </td>
-                            <td> 13 </td>
-                            <td> 14 </td>
-                            <td> 15 </td>
-                            <td> 16 </td>
-                            <td> 17 </td>
-                            <td> 18 </td>
-                            <td> 321 </td>
-                        </tr>
-
-                    </tbody>
-                </table>
-               <br />
-                <table className='table table-striped' aria-labelledby="tabelLabel">
-                    <tbody>
-                        <tr>
-                            <th colSpan="20" scope="colgroup"> Player: </th>
-                            <th ><button className="btn btn-primary" onClick={this.handleOnChange} >Add</button></th>
-                        </tr>
-                    </tbody>
-                    <tbody>
-                        <tr>
-                            <td> ReadyPlayer1</td>
-                            <th scope="col"> 1 </th>
-                            <th scope="col"> 2 </th>
-                            <th scope="col"> 3 </th>
-                            <th scope="col"> 4 </th>
-                            <th scope="col"> 5 </th>
-                            <th scope="col"> 6 </th>
-                            <th scope="col"> 7 </th>
-                            <th scope="col"> 8 </th>
-                            <th scope="col"> 9 </th>
-                            <th scope="col"> 123 </th>
-                            <th scope="col"> 10 </th>
-                            <th scope="col"> 11 </th>
-                            <th scope="col"> 12 </th>
-                            <th scope="col"> 13 </th>
-                            <th scope="col"> 14 </th>
-                            <th scope="col"> 15 </th>
-                            <th scope="col"> 16 </th>
-                            <th scope="col"> 17 </th>
-                            <th scope="col"> 18 </th>
-                            <th scope="col"> 321 </th>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <br/>
+                {this.populateGameHead()}
+                {this.populatePlayer()}
                 <button className="btn btn-primary" onClick={this.onSaveGame}>Save Game</button>
             </div>
         );
@@ -186,7 +181,7 @@ export class NewGame extends Component {
 
     render() {
         let contents = this.state.newGame
-            ? NewGame.populateNewGame()
+            ? this.populateNewGame()
             : <p>Add new game here...<br/><button className="btn btn-primary" onClick={this.isNewGame}>New Game</button></p>;
         return (
             <div>
